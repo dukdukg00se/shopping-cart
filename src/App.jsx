8563d0d34472466635067e87ab67f4bf4ad18f59
Will H <will.wkhu@gmail.com>
2023-06-29 23:56:55 -0700

    Setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Products from './pages/products/Products';
+import About from './pages/about/About';
+import Cart from './pages/cart/Cart';
+import NotFound from './pages/notfound/NotFound';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Test</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
