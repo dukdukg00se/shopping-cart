83f6b71874fd367834925ac5edd80badf9cef58d
Will H <will.wkhu@gmail.com>
2023-07-09 22:59:50 -0700

    Update home page content and styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <main className={styles.home}>
+      <Link className={styles.overlayText} to="/products">
+        <h2>Shop Our Summer Collection</h2>
+      </Link>
+    </main>
   );
 }
