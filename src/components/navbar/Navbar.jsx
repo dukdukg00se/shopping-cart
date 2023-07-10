04a031c962e5b69d553b5b5de19e683d2ecf37c1
Will H <will.wkhu@gmail.com>
2023-07-09 23:01:09 -0700

    Update Navbar content and styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,23 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <header>
-      <h1>The Shop</h1>
-      <nav>
-        <ul className="navList">
-          <li>
+    <header className={styles.header}>
+      <div>
+        <h1>A.B.C</h1>
+        <h2>A Beautiful Choice</h2>
+      </div>
+
+      <nav className={styles.navContainer}>
+        <ul>
+          <li className={styles.linkWrapper}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={styles.linkWrapper}>
             <Link to="/products">Products</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
+          <li className={styles.linkWrapper}>
             <Link to="/cart">Cart</Link>
           </li>
         </ul>
