7f63d0e77373418357695238de0ff2f5375d829b
Will H <will.wkhu@gmail.com>
2023-07-11 00:08:57 -0700

    Remove hooks, change to a presentation component

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,71 +1,43 @@
-import { useEffect, useState } from 'react';
-import ProductCard from '../../components/productcard/ProductCard';
-import './Products.css';
+// import { useEffect, useState } from 'react';
+import Catalog from '../../components/catalog/Catalog';
+import styles from './Products.module.css';
 
-async function loadProducts() {
-  const response = await fetch('https://fakestoreapi.com/products?limit=2');
+export default function Products({ onClick, items }) {
+  // const [products, setProducts] = useState();
+  // const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState();
 
-  if (!response.ok) {
-    throw new Error('Check product fetch');
-  }
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   // async function loadProducts() {
+  //   //   try {
+  //   //     const products = await getImage();
+  //   //     setProducts(products);
+  //   //   } catch (error) {
+  //   //     console.log(error);
+  //   //   }
+  //   // }
+  //   // loadProducts();
 
-  return response.json();
-}
-
-export default function Products() {
-  const [products, setProducts] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState();
-
-  useEffect(() => {
-    setIsLoading(true);
-    // async function loadProducts() {
-    //   try {
-    //     const products = await getImage();
-    //     setProducts(products);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
-    // loadProducts();
-
-    /** Instead of a named fn and calling it as above, use an IIFE */
-    (async () => {
-      try {
-        const products = await loadProducts();
-        setProducts(products);
-      } catch (error) {
-        console.error(error);
-        setError(error);
-      }
-      setIsLoading;
-    })();
-  }, []);
+  //   /** Instead of a named fn and calling it as above, use an IIFE */
+  //   (async () => {
+  //     try {
+  //       const products = await loadProducts();
+  //       setProducts(getClothingItems(products));
+  //     } catch (error) {
+  //       setError('Something went wrong');
+  //     }
+  //     setIsLoading(false);
+  //   })();
+  // }, []);
 
   return (
-    <main>
-      <h1>Products</h1>
-      {/* <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.id} id={product.id} className="productCard">
-              <div>
-                <img src={product.image}></img>
-                <div className="productDescription">
-                  <div className="title">{product.title}</div>
-                  <div className="description">{product.description}</div>
-                  <div className="price">${product.price}</div>
-                </div>
-                <button type="button">Add To Cart</button>
-              </div>
-            </li>
-          );
-        })}
-      </ul> */}
+    <main className={styles.productsContainer}>
+      {/* <h1>Products</h1>
+      {error && <p>ERRRROOORRR</p>} */}
+      {/* {isLoading ? <p>Loading Products</p> : <p>Done Loading</p>} */}
 
-      <ul className="productList">
-        {products.map((prod) => ProductCard(prod))}
-      </ul>
+      {items && <Catalog products={items} onClick={onClick} />}
     </main>
   );
 }
