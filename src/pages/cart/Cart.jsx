9b23d4c06f04b898e4d3168b578688bcfd818e75
Will H <will.wkhu@gmail.com>
2023-07-11 23:19:38 -0700

    Declare fn to get cart total, add navigate fn

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,14 +1,21 @@
 import styles from './Cart.module.css';
+import addIcon from '../../assets/add-icon.svg';
+import removeIcon from '../../assets/remove-icon.svg';
+import { useNavigate } from 'react-router-dom';
 
 function getTotal(arr) {
   let total = 0;
   arr.forEach((item) => {
     total += item.price * item.quantity;
   });
-  return total;
+  return total.toFixed(2);
 }
 
 export default function Cart({ itemsInCart }) {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.cart}>
       <h3>Your Shopping Cart</h3>
@@ -18,15 +25,19 @@ export default function Cart({ itemsInCart }) {
           return (
             <div key={item.id} id={item.id} className={styles.item}>
               <div>
-                <img src={item.image}></img>
+                <img src={item.image} className={styles.itemImg}></img>
               </div>
               <div className={styles.itemInfo}>
                 <h3>{item.title}</h3>
                 <span>{item.price}</span>
                 <div>
-                  <button>-</button>
+                  <button>
+                    <img src={removeIcon} />
+                  </button>
                   <span>{item.quantity}</span>
-                  <button>+</button>
+                  <button>
+                    <img src={addIcon} />
+                  </button>
                 </div>
               </div>
             </div>
@@ -37,7 +48,9 @@ export default function Cart({ itemsInCart }) {
       <h3>Total: ${getTotal(itemsInCart)}</h3>
 
       <button type="button">Checkout</button>
-      <button type="button">Close</button>
+      <button type="button" onClick={goBack}>
+        Close
+      </button>
     </div>
   );
 }
