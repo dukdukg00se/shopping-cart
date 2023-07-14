1777034b01ed0b835056d3ebae4e5a8698a16f57
Will H <will.wkhu@gmail.com>
2023-07-13 23:08:01 -0700

    Consolidate code
    Moved contents of Catalog element and styles into Products element.

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,43 +1,28 @@
-// import { useEffect, useState } from 'react';
-import Catalog from '../../components/catalog/Catalog';
 import styles from './Products.module.css';
 
-export default function Products({ onClick, items }) {
-  // const [products, setProducts] = useState();
-  // const [error, setError] = useState();
-  // const [isLoading, setIsLoading] = useState();
-
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   // async function loadProducts() {
-  //   //   try {
-  //   //     const products = await getImage();
-  //   //     setProducts(products);
-  //   //   } catch (error) {
-  //   //     console.log(error);
-  //   //   }
-  //   // }
-  //   // loadProducts();
-
-  //   /** Instead of a named fn and calling it as above, use an IIFE */
-  //   (async () => {
-  //     try {
-  //       const products = await loadProducts();
-  //       setProducts(getClothingItems(products));
-  //     } catch (error) {
-  //       setError('Something went wrong');
-  //     }
-  //     setIsLoading(false);
-  //   })();
-  // }, []);
-
+export default function Products({ onClick, products }) {
   return (
     <main className={styles.productsContainer}>
-      {/* <h1>Products</h1>
-      {error && <p>ERRRROOORRR</p>} */}
-      {/* {isLoading ? <p>Loading Products</p> : <p>Done Loading</p>} */}
-
-      {items && <Catalog products={items} onClick={onClick} />}
+      {products && (
+        <ul className={styles.catalog}>
+          {products.map((product) => {
+            return (
+              <li key={product.id} id={product.id}>
+                <div className={styles.productCard}>
+                  <img src={product.image}></img>
+                  <div className={styles.productDescription}>
+                    <h4 className={styles.productTitle}>{product.title}</h4>
+                    <div className={styles.productPrice}>${product.price}</div>
+                  </div>
+                  <button type="button" onClick={onClick}>
+                    Add To Cart
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </main>
   );
 }
