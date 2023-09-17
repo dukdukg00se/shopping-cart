256b14285e66a34a21b3a65faab047411063c6b5
Will H <will.wkhu@gmail.com>
2023-09-16 23:31:02 -0700

    Work on tests

--- src/components/cart/Cart.test.jsx
+++ src/components/cart/Cart.test.jsx
@@ -15,7 +15,7 @@ const mockIncrease = vi.fn();
 const mockDecrease = vi.fn();
 const mockCart = [];
 
-test('Has correct headers', () => {
+test('Displays correct component header', () => {
   render(
     <BrowserRouter>
       <Cart
@@ -26,8 +26,83 @@ test('Has correct headers', () => {
     </BrowserRouter>,
   );
 
-  const headers = screen.getAllByRole('heading', { level: 3 });
-  expect(headers.length).toBe(2);
-  expect(headers[0].textContent).toMatch('Your Shopping Cart');
-  expect(headers[1].textContent).toMatch('Total: $0.00');
+  expect(screen.getByRole('heading', { level: 3 }).textContent).toBe(
+    'Your Shopping Cart',
+  );
+});
+
+test('Displays correct price header', () => {
+  render(
+    <BrowserRouter>
+      <Cart
+        itemsInCart={mockCart}
+        increaseQuantity={mockIncrease}
+        decreaseQuantity={mockDecrease}
+      />
+    </BrowserRouter>,
+  );
+
+  expect(screen.getByRole('heading', { level: 4 }).textContent).toBe(
+    'Total: $0.00',
+  );
+});
+
+test('Price header correctly displays price of items in cart', () => {
+  const mockCartWithItems = [
+    {
+      title: 'Mens Casual Premium Slim Fit T-Shirts ',
+      price: 22.3,
+      image:
+        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      quantity: 2,
+    },
+    {
+      title: 'Mens Cotton Jacket',
+      price: 55.99,
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+      quantity: 1,
+    },
+  ];
+
+  render(
+    <BrowserRouter>
+      <Cart
+        itemsInCart={mockCartWithItems}
+        increaseQuantity={mockIncrease}
+        decreaseQuantity={mockDecrease}
+      />
+    </BrowserRouter>,
+  );
+
+  expect(screen.getByRole('heading', { level: 4 }).textContent).toBe(
+    'Total: $100.59',
+  );
+});
+
+test('Cart items', () => {
+  const mockCartWithItems = [
+    {
+      title: 'Mens Casual Premium Slim Fit T-Shirts ',
+      price: 22.3,
+      image:
+        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      quantity: 2,
+    },
+    {
+      title: 'Mens Cotton Jacket',
+      price: 55.99,
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+      quantity: 1,
+    },
+  ];
+
+  render(
+    <BrowserRouter>
+      <Cart
+        itemsInCart={mockCartWithItems}
+        increaseQuantity={mockIncrease}
+        decreaseQuantity={mockDecrease}
+      />
+    </BrowserRouter>,
+  );
 });
